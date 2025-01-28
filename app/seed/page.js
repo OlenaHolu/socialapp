2568f28055f57c670c4efae623da1604062f1455
Olena.Holub <olehol@alu.edu.gva.ua>
2025-01-28 15:37:35 +0000

    fix

--- app/seed/page.js
+++ app/seed/page.js
@@ -1,8 +1,8 @@
 import { sql } from "@vercel/postgres";
 
 export default async () => {
-    await sql`DROP TABLE IF EXISTS POSTS`
-    await sql `CREATE TABLE POSTS(
+    //await sql`DROP TABLE IF EXISTS POSTS`
+    await sql `CREATE TABLE IF NOT EXISTS POSTS(
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         content TEXT,
         url TEXT
